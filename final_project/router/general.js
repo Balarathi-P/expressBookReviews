de069b8b80d11a0efb07a009af352f021ff00517
Balarathi-P <balarathip07@gmail.com>
2025-03-05 09:55:20 -0500

    Completed task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,13 +19,24 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let bookNo = req.params.isbn;
+  
+  res.json(books[bookNo]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+
+  for(let key in books){
+
+    if(books[key]["author"] == author){
+
+        res.json(books[key]);
+        res.end(); 
+    }
+  }
 });
 
 // Get all books based on title
