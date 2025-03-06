26d7952c2f4b5509c63736675ee3203a28a69859
Balarathi-P <balarathip07@gmail.com>
2025-03-05 21:56:21 -0500

    Step 5 completed yet to take screenshot

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -42,13 +42,24 @@ public_users.get('/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title;
+
+  for(let key in books){
+
+    if(books[key]["title"] == title){
+
+        res.json(books[key]);
+        res.end();
+    }
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  res.json(books[isbn]["reviews"]);
+  res.end();
 });
 
 module.exports.general = public_users;
